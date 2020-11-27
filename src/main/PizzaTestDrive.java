78a7a413f7063b7ac2fff354885aa4f67739ddf8
tom.joalland <tom.joalland@gmail.com>
2020-11-27 21:23:23 +0100

    fix #All: Completed all duties

--- src/main/PizzaTestDrive.java
+++ src/main/PizzaTestDrive.java
@@ -8,8 +8,8 @@ public class PizzaTestDrive {
 
 	public static void main(String[] args) {
 
-        Pizzeria boutiqueBrest = PizzeriaFabrique.getInstance().creerPizzeria("Brest");
-        Pizzeria boutiqueStrasbourg = PizzeriaFabrique.getInstance().creerPizzeria("Strasbourg");
+        Pizzeria boutiqueBrest = PizzeriaFactory.getInstance().creerPizzeria("Brest");
+        Pizzeria boutiqueStrasbourg = PizzeriaFactory.getInstance().creerPizzeria("Strasbourg");
 
         Pizza pizza = boutiqueBrest.commanderPizza("fromage");
         System.out.println("JMB a commandé une " + pizza.getNom() + "\n");
